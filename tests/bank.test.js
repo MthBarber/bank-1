7bf10900b2824959a48d45277c5ec94be9050eaa
Matt Barber <mthbarber@gmail.com>
2023-01-23 11:35:30 +0000

    Add more basic tests to Bank class

--- tests/bank.test.js
+++ tests/bank.test.js
@@ -1,14 +1,24 @@
 const Bank = require('../classes/bank');
 
 describe('Testing for the Bank class', ()=> {
+    it("Creates an instance of bank with 0 balance", () => {
+        const bank = new Bank;
+        expect(bank.checkBalance()).toBe(0);
+    });
+
+    it("Creates an instance of bank with 10 balance", () => {
+        const bank = new Bank(10);
+        expect(bank.checkBalance()).toBe(10);
+    });
+
     it('Create an instance of bank that accepts a deposit', ()=> {
-        const bank = new Bank
+        const bank = new Bank;
         const deposit = {
             checkAmount(){
-                return 200
+                return 200;
             }
         }
-        bank.newDeposit(deposit.checkAmount())
-        expect(bank.checkBalance()).toBe(200)
+        bank.newDeposit(deposit.checkAmount());
+        expect(bank.checkBalance()).toBe(200);
     })
 })
